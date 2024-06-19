b4554e2a8c399b398b2aa76e85b1f08636a0e162
Denilson Salas G <denilsonsalguz@gmail.com>
2024-06-19 18:16:29 -0500

    validating company data

--- routes/company/company.js
+++ routes/company/company.js
@@ -1,33 +1,47 @@
 const express = require('express')
+const { validateCompanyData, validateCompanyDataUpdate } = require('../../schemas/company')
 const router = express.Router()
 
 // post: crear nueva empresa ` /company/ `
 router.post('/', function(req, res) {
-  const {ruc, nameCompany, contact,
-    dni, phoneNumber, mail, address, cci, bank
-  } = req.body 
-  console.log(req.body)
+  const validateData = validateCompanyData(req.body)
+  if(validateData.error){
+    return res.status(400).json({error: JSON.parse(validateData.error.message)})
+  }
+  console.log(validateData.data)
+  // const {ruc, nameCompany, contact,
+  //   dni, phoneNumber, email, address, cci, bank
+  // } = req.body 
   res.status(201).json({"message": "crear nueva empresa"})
 })
+
 // get: obtener ruc y razon social ` /company/ `
 router.get('/', function(req, res) {  
   res.status(200).json({"message": "ruc y razon social"})
 })
+
 // get: obtener datos de la empresa por su ruc ` /company/:rucCompany `
 router.get('/:rucCompany', function(req, res) {
   const {rucCompany} = req.params
   console.log(rucCompany)
   res.status(200).json({"message": "obtener datos de la empresa por su ruc"})
 })
+
 // put: actualizar datos de la empresa ` /company/:rucCompany `
 router.put('/:rucCompany', function(req, res) {  
   const {rucCompany} = req.params
-  const {contact, dni, phoneNumber,
-    mail, address, cci, bank} = req.body
-  console.log(rucCompany)
-  console.log(req.body)
+  const validateData = validateCompanyDataUpdate({ruc: rucCompany, ...req.body})
+  if(validateData.error){
+    return res.status(400).json({error: JSON.parse(validateData.error.message)})
+  }
+  console.log(validateData.data)
+  // const {contact, dni, phoneNumber,
+  //   email, address, cci, bank} = req.body
+  // console.log(rucCompany)
+  // console.log(req.body)
   res.status(200).json({"message": "actualizar datos de la empresa"})
 })
+
 // delete: eliminar empresa ` /company/:rucCompany `
 router.delete('/:rucCompany', function(req, res) {
   const {rucCompany} = req.params
---
+++ schemas/company.js
@@ -0,0 +1,27 @@
+const z = require('zod')
+
+const companySchema  = z.object({
+  ruc: z.string().max(11).min(11),
+  nameCompany: z.string(),
+  contact: z.string(),
+  dni: z.string().max(8).min(8),
+  phoneNumber: z.string().max(12).min(9),
+  email: z.string().email(),
+  address: z.string(),
+  cci: z.string().max(20).min(20),
+  bank: z.string(),
+})
+
+//validamos todo el obj
+function validateCompanyData(object) {
+  return companySchema.safeParse(object)
+}
+//validamos partes del obj
+function validateCompanyDataUpdate(object) {
+  return companySchema.partial().safeParse(object)
+}
+
+module.exports ={
+  validateCompanyData,
+  validateCompanyDataUpdate
+}
