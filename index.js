513b3ba2124aed35e5d98b33350b9c9dcda6e990
Denilson Salas G <denilsonsalguz@gmail.com>
2024-06-19 16:44:16 -0500

    starting server on port 1234

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+//routes-module
+const company = require('./routes/company/company')
+
+const PORT = process.env.PORT ?? 1234
+
+const app = express()
+app.disable('x-powered-by')
+
+// midleware
+app.use(function (req, res, next) {
+  console.log(req.method)
+  console.log(req.url)
+  console.log('---')
+  next()
+})
+app.use(express.json())
+
+//routes
+app.use('/company', company)
+
+app.get('/', function (req, res) {
+  res.status(200).send('iniciando RF API')
+})
+
+//route 404 not found
+app.use(function (req, res) {
+  res.sendStatus(404)
+})
+
+//starting server
+app.listen(PORT, ()=>{
+  console.log(`server listenig on port http://localhost:${PORT}`)
+})
