77832be4d07f4636070c6541ec3f346e761f65cd
Denilson Salas G <denilsonsalguz@gmail.com>
2024-06-26 20:59:54 -0500

    adding purchase order controller

---
+++ controller/order-controller.js
@@ -0,0 +1,70 @@
+import { OrderModel } from "../models/order-model.js"
+import {validateOrderPartialData} from '../schemas/order.js'
+export class PurchaseOrderController{
+  static async SavePurchaseOrder(req, res) {
+    const validatedData = validateOrderPartialData(req.body)
+    if(validatedData.error){
+      return res.status(400).json({error: JSON.parse(validatedData.error.message)})
+    }
+    await OrderModel.SavePurchaseOrderData(validatedData)
+    return res.sendStatus(201)
+  }
+  static async GetListOrder(req, res) {
+    let data
+    if(req.url === "/pending"){
+      data = await OrderModel.GetPurchaseOrderList("pending")
+    }else if(req.url === "/delivered"){
+      data = await OrderModel.GetPurchaseOrderList("delivered")
+    }
+    return res.status(200).json(data)
+  }
+  static async GetPaymentTrackingList(req, res) {
+    const data = await OrderModel.GetPaymentTracking()
+    return res.status(200).json(data)
+  }
+  static async GetPurchaseOrderData(req, res) {
+    const {nroOrder} = req.params
+    const validatedData = validateOrderPartialData({numberOrder: nroOrder})
+    if(validatedData.error){
+      return res.status(400).json({error: JSON.parse(validatedData.error.message)})
+    }
+    const data = await OrderModel.GetPurchaseOrderData(validatedData.data)
+    return res.status(200).json(data)
+  }
+  static async UpdatePurchaseOrder (req, res) {
+    const {nroOrder} = req.params
+    const validatedData = validateOrderPartialData({numberOrder: nroOrder, ...req.body})
+    if(validatedData.error){
+      return res.status(400).json({error: JSON.parse(validatedData.error.message)})
+    }
+    await OrderModel.UpdatePurchaseOrderData(validatedData)
+    return res.sendStatus(200)
+  }
+  static async UpdateStateOrder(req, res) {
+    const {nroOrder} = req.params
+    const validatedData = validateOrderPartialData({numberOrder: nroOrder, ...req.body})
+    if(validatedData.error){
+      return res.status(400).json({error: JSON.parse(validatedData.error.message)})
+    }
+    await OrderModel.UpdateStateOrder(validatedData.data)
+    return res.sendStatus(200)
+  }
+  static async UpdateStatePayment(req, res) {
+    const {nroOrder} = req.params
+    const validatedData = validateOrderPartialData({numberOrder: nroOrder, ...req.body})
+    if(validatedData.error){
+      return res.status(400).json({error: JSON.parse(validatedData.error.message)})
+    }
+    await OrderModel.UpdatePaymentStateOrder(validatedData)
+    return res.sendStatus(200)
+  }
+  static async DeleteOrder(req, res) {
+    const {nroOrder} = req.params
+    const validatedData = validateOrderPartialData({numberOrder: nroOrder})
+    if(validatedData.error){
+      return res.status(400).json({error: JSON.parse(validatedData.error.message)})
+    }
+    await OrderModel.DeletePurchaseOrderData(validatedData.data)
+    return res.sendStatus(200)
+  }
+}
--- routes/purchase-order/order.js
+++ routes/purchase-order/order.js
@@ -1,84 +1,31 @@
 import { Router } from 'express'
 import {validateOrderPartialData} from '../../schemas/order.js'
 import { OrderModel } from '../../models/order-model.js'
+import { PurchaseOrderController } from '../../controller/order-controller.js'
 
 const router = Router()
 
 // post: guardar datos iniciales de la orden de compra. `/purchase-order/`
-router.post('/', async function (req, res) {
-  const validatedData = validateOrderPartialData(req.body)
-  if(validatedData.error){
-    return res.status(400).json({error: JSON.parse(validatedData.error.message)})
-  }
-  await OrderModel.SavePurchaseOrderData(validatedData)
-  return res.sendStatus(201)
-})
+router.post('/', PurchaseOrderController.SavePurchaseOrder)
 
 // get: obtener lista de ordenes de compra **pendientes**  no mayor a 30 dias. `/purchase-order/pending`
-router.get('/pending', async function (req, res) {
-  const data = await OrderModel.GetPurchaseOrderList("pending")
-  return res.status(200).json(data)
-})
+router.get('/pending', PurchaseOrderController.GetListOrder)
 
 // get: obtener lista de ordenes de compra **entregados** por mes. `/purchase-order/delivered`
-router.get('/delivered',async function (req, res) {
-  const data = await OrderModel.GetPurchaseOrderList("delivered")
-  return res.status(200).json(data)
-})
+router.get('/delivered', PurchaseOrderController.GetListOrder)
+
 //get: obtener lista de ordenes de compra para seguimiento de pago. `/purchase-order/payment-tracking`
-router.get('/payment-tracking',async function (req, res) {
-  const data = await OrderModel.GetPaymentTracking()
-  return res.status(200).json(data)
-})
+router.get('/payment-tracking', PurchaseOrderController.GetPaymentTrackingList)
+
 //get: obtener datos de la orden de compra segun su **nro de orden**. `/purchase-order/:nroOrder`
-router.get('/:nroOrder',async function (req, res) {
-  const {nroOrder} = req.params
-  const validatedData = validateOrderPartialData({numberOrder: nroOrder})
-  if(validatedData.error){
-    return res.status(400).json({error: JSON.parse(validatedData.error.message)})
-  }
-  const data = await OrderModel.GetPurchaseOrderData(validatedData.data)
-  return res.status(200).json(data)
-})
+router.get('/:nroOrder', PurchaseOrderController.GetPurchaseOrderData)
 //put: actualizar los datos de la orden de compra. `/purchase-order/:nroOrder`
-router.put('/:nroOrder',async function (req, res) {
-  const {nroOrder} = req.params
-  const validatedData = validateOrderPartialData({numberOrder: nroOrder, ...req.body})
-  if(validatedData.error){
-    return res.status(400).json({error: JSON.parse(validatedData.error.message)})
-  }
-  await OrderModel.UpdatePurchaseOrderData(validatedData)
-  return res.sendStatus(200)
-})
+router.put('/:nroOrder', PurchaseOrderController.UpdatePurchaseOrder)
 //patch: actualizar el estado de la orden de compra. `/purchase-order/state/:nroOrder`
-router.patch('/state/:nroOrder',async function (req, res) {
-  const {nroOrder} = req.params
-  const validatedData = validateOrderPartialData({numberOrder: nroOrder, ...req.body})
-  if(validatedData.error){
-    return res.status(400).json({error: JSON.parse(validatedData.error.message)})
-  }
-  await OrderModel.UpdateStateOrder(validatedData.data)
-  return res.sendStatus(200)
-})
+router.patch('/state/:nroOrder', PurchaseOrderController.UpdateStateOrder)
 //patch: actualizar el estaddo de pago. `/purchase-order/state-payment/:nroOrder`
-router.patch('/state-payment/:nroOrder', async function (req, res) {
-  const {nroOrder} = req.params
-  const validatedData = validateOrderPartialData({numberOrder: nroOrder, ...req.body})
-  if(validatedData.error){
-    return res.status(400).json({error: JSON.parse(validatedData.error.message)})
-  }
-  await OrderModel.UpdatePaymentStateOrder(validatedData)
-  return res.sendStatus(200)
-})
+router.patch('/state-payment/:nroOrder', PurchaseOrderController.UpdateStatePayment)
 //delete: eliminar la orden de compra. `/purchase-order/:nroOrder`
-router.delete('/:nroOrder',async function (req, res) {
-  const {nroOrder} = req.params
-  const validatedData = validateOrderPartialData({numberOrder: nroOrder})
-  if(validatedData.error){
-    return res.status(400).json({error: JSON.parse(validatedData.error.message)})
-  }
-  await OrderModel.DeletePurchaseOrderData(validatedData.data)
-  return res.sendStatus(200)
-})
+router.delete('/:nroOrder', PurchaseOrderController.DeleteOrder)
 
 export default router
